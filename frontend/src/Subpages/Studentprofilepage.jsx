14d7d3bb2c55620615065a5b1a4900d776f60aa9
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-06 21:29:13 +0530

    Update Studentprofilepage.jsx

--- frontend/src/Subpages/Studentprofilepage.jsx
+++ frontend/src/Subpages/Studentprofilepage.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Studentprofilepage = () => {
+  const [student, setStudent] = useState(null)
+  const location = useLocation();
+  const rollNo = location.state?.rollNo;
+  
+  useEffect(() => {
+    if (rollNo) {
+      axios.get(`http://localhost:3000/student/${rollNo}`)
+        .then(res => {
+          setStudent(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }, [rollNo]);
+
+  if(!student) { return <div>Loading...</div> }
+
   return (
     <div className="flex border rounded-lg bg-sky-100 shadow-md overflow-hidden">
       <div className="w-1/3 p-8">
       <img 
-          src="https://github.com/P47Parzival/Tango-Chatapp/blob/main/Frontend/src/assests/face%20(1).png?raw=true" 
+          src={student.profileImageUrl} 
           alt="Student" 
           className="w-full h-auto object-cover rounded"
           style={{
@@ -20,47 +41,47 @@ const Studentprofilepage = () => {
         />
       </div>
       <div className="w-2/3 p-4">
-        <h2 className="text-xl font-bold mb-4">Dhruv Bharatkumar Mali</h2>
+        <h2 className="text-xl font-bold mb-4">{student.Fullname}</h2>
         <div className="space-y-2">
           <div className="flex">
             <span className="font-semibold w-1/2">Student Status:</span>
             <span className="w-1/2">Active</span>
           </div>
           <div className="flex">
-            <span className="font-semibold w-1/2">Admission No.:</span>
-            <span className="w-1/2">230184</span>
+            <span className="font-semibold w-1/2">Admission no:</span>
+            <span className="w-1/2">{student.admissionNo}</span>
           </div>
           <div className="flex">
-            <span className="font-semibold w-1/2">Admission Year:</span>
-            <span className="w-1/2">2023-24</span>
+            <span className="font-semibold w-1/2">Admission year:</span>
+            <span className="w-1/2">{student.admissionYear}</span>
           </div>
           <div className="flex">
-            <span className="font-semibold w-1/2">Roll No.:</span>
-            <span className="w-1/2">1AUA23BCS040</span>
+            <span className="font-semibold w-1/2">Roll no.:</span>
+            <span className="w-1/2">{student.rollNo}</span>
           </div>
           <div className="flex">
             <span className="font-semibold w-1/2">Degree:</span>
-            <span className="w-1/2">Undergraduate</span>
+            <span className="w-1/2">{student.degree}</span>
           </div>
           <div className="flex">
             <span className="font-semibold w-1/2">Department:</span>
-            <span className="w-1/2">Faculty of Engineering Sciences and Technology</span>
+            <span className="w-1/2">{student.department}</span>
           </div>
           <div className="flex">
             <span className="font-semibold w-1/2">Semester:</span>
-            <span className="w-1/2">SEMESTER - III</span>
+            <span className="w-1/2">{student.semester}</span>
           </div>
           <div className="flex">
             <span className="font-semibold w-1/2">Course Name:</span>
-            <span className="w-1/2">B. Tech. in Computer Science and Engineering (AI-ML)</span>
+            <span className="w-1/2">{student.coursename}</span>
           </div>
           <div className="flex">
             <span className="font-semibold w-1/2">College:</span>
-            <span className="w-1/2">ADANI UNIVERSITY</span>
+            <span className="w-1/2">{student.college}</span>
           </div>
           <div className="flex">
             <span className="font-semibold w-1/2">Curriculum Plan:</span>
-            <span className="w-1/2">BTech-CSE(AI-ML)</span>
+            <span className="w-1/2">{student.curriculumPlan}</span>
           </div>
         </div>
       </div>
