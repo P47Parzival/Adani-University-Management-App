ed0ea298f74ef416a006bbd59b67b13dac07b868
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-06 21:27:05 +0530

    Update Studentheader.jsx

--- frontend/src/Components/Studentheader.jsx
+++ frontend/src/Components/Studentheader.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useLocation } from 'react-router-dom'
 
 const Studentheader = () => {
+  const [student, setStudent] = useState(null);
+  const location = useLocation();
+  const rollNo = location.state?.rollNo;
+
+  useEffect(() => {
+    if (rollNo) {
+      axios.get(`http://localhost:3000/student/${rollNo}`)
+        .then(res => {
+          setStudent(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }, [rollNo]);
+
+  if(!student) { return <div>Loading...</div> }
+
   return (
     <div className='h-[86px] p-4 flex justify-between items-center bg-gray-200'>
       <div>
@@ -9,7 +30,7 @@ const Studentheader = () => {
       <div className='flex items-center space-x-4'> 
         <div className='flex items-center space-x-2'>
             <img className='h-8 rounded-full' src="https://github.com/P47Parzival/Tango-Chatapp/blob/main/Frontend/src/assests/face%20(1).png?raw=true" alt="" />
-            <h1>Dhruv Bharatkumar Mali</h1>
+            <h1>{student.Fullname}</h1>
         </div>
         <button className='border-2 border-black p-2 rounded-full'>Logout</button>
       </div>
