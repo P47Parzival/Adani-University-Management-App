ce707929d90339949171e485afff6350ddb6174b
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-08 12:39:20 +0530

    Update Admindashboardpage.jsx

--- frontend/src/Pages/Admindashboardpage.jsx
+++ frontend/src/Pages/Admindashboardpage.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import Adminheader from '../Components/Adminheader'
-import Adminsidebar from '../Components/Adminsidebar'
-import Adminprofilepage from '../Subpages/Adminprofilepage'
+import React, { useState } from 'react';
+import Adminheader from '../Components/Adminheader';
+import Adminsidebar from '../Components/Adminsidebar';
+import Adminprofilepage from '../Subpages/Admin/Adminprofilepage';
 
 const Admindashboardpage = () => {
-    return (
-        <div>
-            <Adminheader></Adminheader>
-            <div className='flex justify-center pt-4'>
-                <div className='w-1/3 pl-10 '>
-                    <Adminsidebar />
-                </div>
-                <div className='w-2/3 pr-10'>
-                    <Adminprofilepage />
-                </div>
-            </div>
-        </div>
-    )
-}
+  const [activePage, setActivePage] = useState('Profile');
 
-export default Admindashboardpage
+  return (
+    <div>
+      <Adminheader />
+      <div className='flex justify-center pt-4'>
+        <div className='w-1/3 pl-10'>
+          <Adminsidebar setActivePage={setActivePage} activePage={activePage} />
+        </div>
+        <div className='w-2/3 pr-10'>
+          {activePage === 'Profile' && <Adminprofilepage />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Admindashboardpage;
