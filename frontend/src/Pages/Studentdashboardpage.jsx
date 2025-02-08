0a58aee06b8acffdc373f819cacdab7306647999
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-08 12:39:37 +0530

    Update Studentdashboardpage.jsx

--- frontend/src/Pages/Studentdashboardpage.jsx
+++ frontend/src/Pages/Studentdashboardpage.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import Studentheader from '../Components/Studentheader'
-import Studentsidebar from '../Components/Studentsidebar'
-import Studentprofilepage from '../Subpages/Studentprofilepage'
+import React, { useState } from 'react';
+import Studentheader from '../Components/Studentheader';
+import Studentsidebar from '../Components/Studentsidebar';
+import Studentprofilepage from '../Subpages/Student/Studentprofilepage';
+import Studentattendancepage from '../Subpages/Student/Studentattendancepage';
+import Studentbillingpage from '../Subpages/Student/Studentbillingpage';
+import Studentresultpage from '../Subpages/Student/Studentresultpage';
+import Studentcalenderpage from '../Subpages/Student/Studentcalenderpage'
 
 const Studentdashboardpage = () => {
+    const [activePage, setActivepage] = useState('Profile');
+
     return (
         <div>
             <Studentheader />
             <div className='flex justify-center pt-4'>
                 <div className='w-1/3 pl-10 '>
-                    <Studentsidebar />
+                    <Studentsidebar setActivepage={setActivepage} activePage={activePage} />
                 </div>
                 <div className='w-2/3 pr-10'>
-                    <Studentprofilepage />
+                    {activePage === 'Profile' && <Studentprofilepage />}
+                    {activePage === 'Attendance' && <Studentattendancepage />}
+                    {activePage === 'Billing' && <Studentbillingpage />}
+                    {activePage === 'Results' && <Studentresultpage />}
+                    {activePage === 'Calendar' && <Studentcalenderpage />}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Studentdashboardpage
+export default Studentdashboardpage;
+
