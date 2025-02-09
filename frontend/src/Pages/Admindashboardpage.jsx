79a542f842bda42f6534876fd04ffe9ec5e28290
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-09 21:48:58 +0530

    Update Admindashboardpage.jsx

--- frontend/src/Pages/Admindashboardpage.jsx
+++ frontend/src/Pages/Admindashboardpage.jsx
@@ -2,19 +2,27 @@ import React, { useState } from 'react';
 import Adminheader from '../Components/Adminheader';
 import Adminsidebar from '../Components/Adminsidebar';
 import Adminprofilepage from '../Subpages/Admin/Adminprofilepage';
+import Adminmanagestudentspage from '../Subpages/Admin/Adminmanagestudentspage';
+import Adminmanagefaculty from '../Subpages/Admin/Adminmanagefacultypage';
+import Adminreports from '../Subpages/Admin/Adminreportspage';
+import Adminsettings from '../Subpages/Admin/Adminsettingspage';
 
 const Admindashboardpage = () => {
-  const [activePage, setActivePage] = useState('Profile');
+  const [activePage, setActivepage] = useState('Profile');
 
   return (
     <div>
       <Adminheader />
       <div className='flex justify-center pt-4'>
         <div className='w-1/3 pl-10'>
-          <Adminsidebar setActivePage={setActivePage} activePage={activePage} />
+          <Adminsidebar setActivepage={setActivepage} activePage={activePage} />
         </div>
         <div className='w-2/3 pr-10'>
           {activePage === 'Profile' && <Adminprofilepage />}
+          {activePage === 'Manage Students' && <Adminmanagestudentspage />}
+          {activePage === 'Manage Faculty' && <Adminmanagefaculty />}
+          {activePage === 'Reports' && <Adminreports />}
+          {activePage === 'Settings' && <Adminsettings />}
         </div>
       </div>
     </div>
