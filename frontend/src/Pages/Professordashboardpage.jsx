e5a110ff0a2318d3457395d5d646f99c5db52fbd
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-10 21:57:12 +0530

    Update Professordashboardpage.jsx

--- frontend/src/Pages/Professordashboardpage.jsx
+++ frontend/src/Pages/Professordashboardpage.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Adminheader from '../Components/Adminheader'
-import Adminsidebar from '../Components/Adminsidebar'
-import Professorprofilepage from '../Subpages/Professorprofilepage'
+import React, { useState } from 'react';
+import Professorheader from '../Components/Professorheader';
+import Professorsidebar from '../Components/Professorsidebar';
+import Professorprofilepage from '../Subpages/Professor/Professorprofilepage';
 
-const Professordashboardpage = () => {
-    return (
-        <div>
-            <Adminheader></Adminheader>
-            <div className='flex justify-center pt-4'>
-                <div className='w-1/3 pl-10 '>
-                    <Adminsidebar />
-                </div>
-                <div className='w-2/3 pr-10'>
-                    <Professorprofilepage />
-                </div>
-            </div>
+
+const ProfessorDashboardPage = () => {
+  const [activePage, setActivePage] = useState('Dashboard');
+
+  return (
+    <div>
+      <Professorheader />
+      <div className='flex justify-center pt-4'>
+        <div className='w-1/3 pl-10'>
+          <Professorsidebar setActivePage={setActivePage} activePage={activePage} />
         </div>
-    )
-}
+        <div className='w-2/3 pr-10'>
+          {activePage === 'Profile' && <Professorprofilepage />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Professordashboardpage
+export default ProfessorDashboardPage;
