23a67fb7fdc79a9bf50f9f7d8fff87ff6ef95175
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-17 13:30:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Landingpage from './Pages/Landingpage';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Studentdashboardpage from './Pages/Studentdashboardpage';
-import Admindashboardpage from './Pages/Admindashboardpage';
-import Professordashboardpage from './Pages/Professordashboardpage';
+import React from 'react'
+import Landingpage from './Pages/Landingpage'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Studentdashboardpage from './Pages/Studentdashboardpage'
+import Admindashboardpage from './Pages/Admindashboardpage'
+import Professordashboardpage from './Pages/Professordashboardpage'
 import { ThemeProvider } from './context/ThemeContext';
 
 const App = () => {
   return (
     <ThemeProvider>
-      <div className="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-white transition-colors duration-200">
+      <div className="min-h-screen dark:bg-gray-900 transition-colors duration-200">
         <Router>
           <Routes>
             <Route path="/" element={<Landingpage />} />
@@ -20,7 +20,7 @@ const App = () => {
         </Router>
       </div>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
