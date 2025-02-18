4cd32135bd0c148b118b7f4c54c732881c0f2571
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-18 19:42:08 +0530

    Update Attendance.js

--- backend/Model/Attendance.js
+++ backend/Model/Attendance.js
@@ -1,42 +1,26 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  fullname: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["Student", "Professor"], required: true },
-}, { timestamps: true });
-
-const User = mongoose.model("User", userSchema);
-module.exports = User;
-
-const subjectSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    professorId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Reference to Professor
+const AttendanceSchema = new mongoose.Schema({
+  rollNo: {
+    type: String,
+    required: true
+  },
+  Fullname: {
+    type: String,
+    required: true
+  },
+  isPresent: {
+    type: Boolean,
+    required: true
+  },
+  semester: {
+    type: String, // Optional: Helps filter attendance records by semester
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now // Stores the date of attendance marking
+  }
 });
-  
-const Subject = mongoose.model("Subject", subjectSchema);
-module.exports = Subject;
 
-const enrollmentSchema = new mongoose.Schema({
-    studentId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    subjectId: { type: mongoose.Schema.Types.ObjectId, ref: "Subject", required: true },
-});
-  
-const Enrollment = mongoose.model("Enrollment", enrollmentSchema);
-module.exports = Enrollment;
-
-const attendanceSchema = new mongoose.Schema({
-  subject: { type: String, required: true },
-  year: { type: String, required: true },
-  section: { type: String, required: true },
-  date: { type: Date, required: true },
-  records: [{
-    rollNo: { type: String, required: true },
-    name: { type: String, required: true },
-    isPresent: { type: Boolean, required: true }
-  }]
-}, { timestamps: true });
-
-const Attendance = mongoose.model('Attendance', attendanceSchema);
-module.exports = Attendance;
+module.exports = mongoose.model('Attendance', AttendanceSchema);
